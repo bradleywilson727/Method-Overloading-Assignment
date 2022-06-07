2f37a62c1efc5f37552285327ee8680e60bd0f40
Bradley Wilson <bradleywilson727@gmail.com>
2022-06-07 11:48:59 -0500

    completed assignment

---
+++ MethodOverloadingAssignment/MethodOverloadingAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MethodOverloadingAssignment
+{
+    class Program
+    {
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool trueOrFalse)
+        {
+            var sum = num1 + num2;
+
+            if(trueOrFalse == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if(trueOrFalse == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if(trueOrFalse == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
